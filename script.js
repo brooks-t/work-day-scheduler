b82e1bc5b0c13bb4fd194da9204ae821b6ac3e9e
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-04-01 17:47:57 -0700

    added first 9am row, working on text area, button, storage functionality

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 var currentDay = $('#currentDay');
 var now = moment();
+var hour = parseInt(now.format('H'));
+var saveButton = $('#nine-am-save-btn');
+
+/*var saveDescription = function (event) {
+    localStorage.setItem()
+}*/
 
 currentDay.text(now.format('dddd, MMMM Do'));
+
+saveButton.on('click', function () {
+    var description = $('textarea[name="9am"]').val();
+    console.log(description);
+    localStorage.setItem("9am", description);
+})
+
+
+
+
+
